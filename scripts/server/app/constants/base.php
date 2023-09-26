2c1f4775bebd69f98e58561b75520ea1551330cf
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-09-26 13:16:37 +0700

    feat : docker build script

--- scripts/server/app/constants/base.php
+++ scripts/server/app/constants/base.php
@@ -1,7 +1,7 @@
 <?php
 
 // DATABASE
-define('DB_SERVER', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', '');
+define('DB_SERVER', 'ep-wandering-sound-44516679.ap-southeast-1.aws.neon.tech');
+define('DB_USERNAME', 'mikeleo03');
+define('DB_PASSWORD', '47EevsXYILOx');
+define('DB_DATABASE', 'neondb');
