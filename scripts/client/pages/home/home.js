fc2dee4052559b0a3b9bcac6a83987ca9604018c
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-09-27 20:51:37 +0700

    feat : basic routing, homepage, and assets

---
+++ scripts/client/pages/home/home.js
@@ -0,0 +1,51 @@
+var products;
+
+function initPage() {
+    infoNavbarAdded();
+}
+
+function infoNavbarAdded() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+            let res = JSON.parse(this.responseText);
+            uname = document.getElementById("uname");
+            if (res['status']) {
+                if (res['data'].isAdmin) {
+                    document.getElementById("unameadmin").innerHTML = res['data'].username;
+                } else {
+                    document.getElementById("unameuser").innerHTML = res['data'].username;
+                }
+                putNavbar(res['data'].isAdmin);
+            } else {
+                uname.innerHTML = "Guest";
+                document.getElementById("loginout").innerHTML = "Login";
+                putNavbar(false);
+            }
+        }
+    };
+    xhttp.open("GET","http://localhost:8000/api/auth/info",true);
+    xhttp.setRequestHeader("Accept", "application/json");
+    xhttp.withCredentials = true;
+    xhttp.send();
+}
+
+function logout() {
+    if (document.getElementById("logout").innerHTML == "Login") {
+        window.location.href = "http://localhost:8080/pages/login/login.html";
+    } else {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200) {
+                let res = JSON.parse(this.responseText);
+                if (res['status']) {
+                    window.location.href = "http://localhost:8080/pages/home/home.html";
+                }
+            }
+        };
+        xhttp.open("POST","http://localhost:8000/api/auth/logout",true);
+        xhttp.setRequestHeader("Accept", "application/json");
+        xhttp.withCredentials = true;
+        xhttp.send();
+    }
+}
