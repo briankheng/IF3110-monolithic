620954d1bc43d33efaf9a293066289c50db6f7cc
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-10-01 23:08:05 +0700

    refactor : basic templating structure

--- scripts/client/pages/template/navbar.php
+++ scripts/client/pages/template/navbar.php
@@ -2,30 +2,30 @@
     <div class="navCt" id="navCt">
         <div class="navLeft">
             <div class="navLogoSide">
-                <img class="navLogo" src="../../assets/logo.png" onclick="redirectToHome()"/>
+                <img class="navLogo" src="../../public/images/logo.png" onclick="redirectToHome()"/>
                 <h1 class="navTitle">&nbsp;KBL</h1>
             </div>
             <div class="navSearch">
-                <img class="navSearchIcon" id="productqueryimg" src="../../assets/search-black.png" onclick="searchProduct()"/>
+                <img class="navSearchIcon" id="productqueryimg" src="../../public/images/search-black.png" onclick="searchProduct()"/>
                 <input class="navSearchInput" type="text" id="queryproduct" placeholder="&nbsp;Search for products or categories" >
             </div>
         </div>
         <div class="navRight">
             <div class="navItems" onclick="redirectToProduct()">
-                <img class="navIcon" src="../../assets/product.png"/>
+                <img class="navIcon" src="../../public/images/product.png"/>
                 <span class="navText">Product</span>
             </div>
             <div class="navItems" onclick="redirectToHistory()">
-                <img class="navIcon" src="../../assets/history.png"/>
+                <img class="navIcon" src="../../public/images/history.png"/>
                 <span class="navText">History</span>
             </div>
             <div class="navItems" onclick="redirectToTopup()">
-                <img class="navIcon" src="../../assets/topup.png"/>
+                <img class="navIcon" src="../../public/images/topup.png"/>
                 <span class="navText">Topup</span>
             </div>
             <div class="navCollapse">
                 <div class="navItems">
-                    <img class="navIcon" src="../../assets/user.png"/>
+                    <img class="navIcon" src="../../public/images/user.png"/>
                     <span class="navText" id="unameuser"></span>
                     <i class="fa fa-caret-down"></i>
                 </div>
@@ -38,30 +38,30 @@
     <div class="navCtAdmin" id="navCtAdmin">
         <div class="navLeft">
             <div class="navLogoSide">
-                <img class="navLogo" src="../../assets/logo.png" onclick="redirectToHome()"/>
+                <img class="navLogo" src="../../public/images/logo.png" onclick="redirectToHome()"/>
                 <h1>&nbsp;KBL</h1>
             </div>
             <div class="navSearch">
-                <img class="navSearchIcon" id="productqueryimg" src="../../assets/search-black.png" onclick="searchProduct()"/>
+                <img class="navSearchIcon" id="productqueryimg" src="../../public/images/search-black.png" onclick="searchProduct()"/>
                 <input class="navSearchInput" type="text" id="queryproduct" placeholder="&nbsp;Search for products or categories" >
             </div>
         </div>
         <div class="navRight">
             <div class="navItems" onclick="redirectToEditProduct()">
-                <img class="navIcon" src="../../assets/edit-product.png"/>
+                <img class="navIcon" src="../../public/images/edit-product.png"/>
                 <span class="navText">Edit Product</span>
             </div>
             <div class="navItems" onclick="redirectToHandleTopup()">
-                <img class="navIcon" src="../../assets/topup.png"/>
+                <img class="navIcon" src="../../public/images/topup.png"/>
                 <span class="navText">Handle Topup</span>
             </div>
             <div class="navItems" onclick="redirectToListUsers()">
-                <img class="navIcon" src="../../assets/user-manage.png"/>
+                <img class="navIcon" src="../../public/images/user-manage.png"/>
                 <span class="navText">Users</span>
             </div>
             <div class="navCollapse">
                 <div class="navItems">
-                    <img class="navIcon" src="../../assets/user.png"/>
+                    <img class="navIcon" src="../../public/images/user.png"/>
                     <span class="navText" id="unameadmin"></span>
                     <i class="fa fa-caret-down"></i>
                 </div>
