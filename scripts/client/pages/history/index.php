7d7b2be9697dacfb7205993c7bb727bc6d332fd9
billc27 <110593711+billc27@users.noreply.github.com>
2023-10-05 02:29:56 +0700

    style: change history page style

--- scripts/client/pages/history/index.php
+++ scripts/client/pages/history/index.php
@@ -5,10 +5,19 @@
     <div class="history-ctn">
         <div class="history-title">History</div>
         <hr class="separator">
-        <div>
-            <div id="history-grid">
+        <div class="buy-history">
+            <div class="buy-history-title">Buy History</div>
+            <div id="buy-history-grid">
                 <div class="col-name">Date</div>
-                <div class="col-name">Nominal</div>
+                <div class="col-name">Total</div>
+            </div>
+        </div>
+        <hr class="table-separator">
+        <div class="topup-history">
+            <div class="topup-history-title">Top Up</div>
+            <div id="topup-history-grid">
+                <div class="col-name">Date</div>
+                <div class="col-name">Amount</div>
             </div>
         </div>
     </div>
