849c7f58ca740d1db8df82aa050a4d5bb7d584d7
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-10-06 00:43:28 +0700

    feat : basic layout for topup page

--- scripts/client/pages/topup/index.php
+++ scripts/client/pages/topup/index.php
@@ -2,7 +2,32 @@
 <?php require_once __DIR__ . "/../template/navbar.php" ?>
 
 <section id="topup">
-
+    <div class="topupContainer">
+        <div class="historyTopupSec">
+            <div class="toupUpReq">
+                <div class="topuptitle">Pending</div>
+                <div class="topupgrid1">
+                    <div class="col-name">Date</div>
+                    <div class="col-name">Amount</div>
+                </div>
+            </div>
+            <div class="toupUpAcc">
+                <div class="topuptitle">History</div>
+                <div class="topupgrid2">
+                    <div class="col-name">Date</div>
+                    <div class="col-name">Status</div>
+                    <div class="col-name">Amount</div>
+                </div>
+            </div>
+        </div>
+        <div class="mainTopupSec">
+            <div class="topuplogo"></div>
+            <div class="toupUpSec">
+                <div class="topupSectitle">Insert the amount</div>
+                <div class="topupinput"></div>
+            </div>
+        </div>
+    </div>
 </section>
 
 <?php require_once __DIR__ . "/../template/footer.php" ?>
