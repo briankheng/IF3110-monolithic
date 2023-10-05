0e63f894bc6e7a4e68495b9b81004f632ae7657b
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-10-05 15:42:07 +0700

    refactor : navbar structure on role-based

--- scripts/client/public/js/navbar.js
+++ scripts/client/public/js/navbar.js
@@ -1,6 +1,3 @@
-let username = undefined;
-let role = undefined;
-
 function initPage() {
     infoNavbarAdded();
 }
@@ -10,24 +7,19 @@ function infoNavbarAdded() {
     xhttp.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200) {
             let res = JSON.parse(this.responseText);
-            uname = document.getElementById("unameuser");
             if (res['status']) {
                 // Role-based navbar
                 if (res['data'].role == 'admin') {
-                    role = "admin";
-                    username = res['data'].username;
-                    document.getElementById("unameadmin").innerHTML = res['data'].username;
                     putNavbar("admin");
                 } else {
-                    role = "user";
-                    username = res['data'].username;
-                    document.getElementById("unameuser").innerHTML = res['data'].username;
                     putNavbar("user");
                 }
+                document.getElementById("unameuser").innerHTML = res['data'].username;
             } else {
-                uname.innerHTML = "Guest";
-                document.getElementById("logout").innerHTML = "Login";
                 putNavbar("guest");
+                document.getElementById("unameuser").innerHTML = "Guest";
+                document.getElementById("logout").innerHTML = "Login";
+                document.getElementById("set").style.display = "none";
             }
         }
     };
@@ -72,14 +64,7 @@ function searchProducts() {
 }
 
 function searchProducts() {
-    var userRole = role;
-    var query;
-
-    if (userRole === 'user') {
-        query = document.getElementById("queryproduct1").value;
-    } else if (userRole === 'admin') {
-        query = document.getElementById("queryproduct2").value;
-    }
+    query = document.getElementById("queryproduct").value;
     window.location.href = "http://localhost:8000/pages/product?query=" + query;
 }
 
@@ -101,18 +86,7 @@ const debouncedSearchProducts = debounce(searchProducts, 300);
 // Call debouncedSearchProducts when the search icon is clicked
 document.getElementById('productqueryimg').addEventListener('click', debouncedSearchProducts);
 
-document.getElementById("queryproduct1")
-    .addEventListener("keyup", function(event) {
-    // console.log("searching");
-    event.preventDefault();
-    // If the user presses the "Enter" key on the keyboard
-    if (event.keyCode == 13) {
-        // Trigger the button element with a click
-        document.getElementById("productqueryimg").click();
-    }
-});
-
-document.getElementById("queryproduct2")
+document.getElementById("queryproduct")
     .addEventListener("keyup", function(event) {
     // console.log("searching");
     event.preventDefault();
