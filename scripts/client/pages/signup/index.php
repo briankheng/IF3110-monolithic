29ce1225c0a25748732ebbf2dd9069d22dc3d9d6
billc27 <110593711+billc27@users.noreply.github.com>
2023-10-05 16:58:22 +0700

    feat: responsive design for signup page

--- scripts/client/pages/signup/index.php
+++ scripts/client/pages/signup/index.php
@@ -7,20 +7,20 @@
 
 <section id="signup">
     <div class="reg-ctn">
-        <div class="reg-title">Create an Account</div>
+        <h1 class="reg-title">Create an Account</h1>
         <div class="reg-form-ctn">
-            <form id="register-form" method="post" action="index.php">
+            <form id="register-form" method="post" action="index.php" role="form">
                 <label for="username">USERNAME <span class="required-field">*</span></label>
-                <input type="text" id="username" name="username">
+                <input type="text" id="username" name="username" aria-required="true">
 
                 <label for="password">PASSWORD <span class="required-field">*</span></label>
-                <input type="password" id="password" name="password">
+                <input type="password" id="password" name="password" aria-required="true">
 
                 <label for="name">DISPLAY NAME <span class="required-field">*</span ></label>
-                <input type="text" id="name" name="name">
+                <input type="text" id="name" name="name" aria-required="true">
 
                 <!-- Error Message -->
-                <div id="error-message"></div>
+                <div id="error-message" role="alert"></div>
 
                 <button type="submit">Sign Up</button>
             </form>
